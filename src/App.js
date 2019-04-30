b6b09058b7e20baa2eb6f21fbcfa4a0e6e93fe23
lbssone <loveseo1124@gmail.com>
2019-04-30 23:09:25 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Form from "./components/Form";
+import Recipes from "./components/Recipes";
+
+const API_KEY = '4144f5adee5c9ac516f59ae1fbc0977f';
+
+class App extends Component {
+  state = {
+    recipes: []
+  }
+  getRecipe = async (e) => {
+    const recipeName = e.target.elements.recipeName.value //拿到submit的內容
+    e.preventDefault(); //沒加這行的話console那邊會因為一直被refresh所以顯示的東西會一閃即逝
+    const api_call = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=30`);
+    
+    const data = await api_call.json();
+    this.setState({ recipes: data.recipes })
+    console.log(this.state.recipes)
+  }
+  componentDidMount = () => {
+    const json = localStorage.getItem("recipes");
+    const recipes = JSON.parse(json);
+    this.setState({ recipes: recipes }); //如果兩個名字一樣可以直接用this.setState({ recipes })
+  }
+  componentDidUpdate = () => {
+    const recipes = JSON.stringify(this.state.recipes);
+    localStorage.setItem("recipes", recipes);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Recipe Search</h1>
+        </header>
+        <Form getRecipe={this.getRecipe} />
+        <Recipes recipes={this.state.recipes}/>
+      </div>
+    );
+  }
 }
 
 export default App;
